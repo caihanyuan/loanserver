68017eaab575981ad1897f0a34d1290908e75325
CAIHANYUAN <caihanyuan@wps.cn>
2018-01-08 17:27:41 +0800

    banner接口调整为get请求

--- route.js
+++ route.js
@@ -3,6 +3,6 @@ exports.setRequestUrl=function(app){
     var banner = require('./models/banner');
     var loanstatus = require('./models/loanStatus');
 
-    app.post('/api/content/v1/banners', banner.getBannerData);
+    app.get('/api/content/v1/banners', banner.getBannerData);
     app.post('/loanstatus', loanstatus.getRandomStatus);
 }
