a5e6964b00bf359abf1a3f9beca3e63fc9c5db49
CAIHANYUAN <caihanyuan@wps.cn>
2018-01-08 14:28:44 +0800

    括号写成了中文字符

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV == "development") {
             error: err
         });
     });
-} else if（ process.env.NODE_ENV == "production"） {
+} else if (process.env.NODE_ENV == "production") {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
